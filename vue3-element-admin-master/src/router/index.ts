8420f8ec70b714de9d0dcc10fb49bfec0e23ea09
cheng666669 <1035107202@qq.com>
2025-07-08 16:48:55 +0800

    列表

--- vue3-element-admin-master/src/router/index.ts
+++ vue3-element-admin-master/src/router/index.ts
@@ -89,7 +89,6 @@ export const constantRoutes: RouteRecordRaw[] = [
         meta: { title: "房号管理", icon: "user", hidden: true },
       },
       {
-
         path: "getcustomerlist",
         name: "getcustomerlist",
         component: () => import("@/views/Customers/GetCustomerListView.vue"),
@@ -142,7 +141,6 @@ export const constantRoutes: RouteRecordRaw[] = [
         name: "ReserverDetails",
         component: () => import("@/views/ResrveRoom/ReserverDetails.vue"),
         meta: { title: "订单详情", keepAlive: true, icon: "user", hidden: true },
-
       },
       {
         path: "accountview",
@@ -162,6 +160,7 @@ export const constantRoutes: RouteRecordRaw[] = [
         component: () => import("@/views/Setting/OperationLog.vue"),
         meta: { title: "操作日志管理", icon: "user", hidden: true },
       },
+     
     ],
   },
 ];
